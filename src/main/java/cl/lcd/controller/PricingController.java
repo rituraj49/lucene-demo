4a86766ecceedca0ba37109a9119482f288307e9
Dhananjay <mr.dhananjaykr2003@gmail.com>
2025-07-02 19:13:46 +0530

    swagger Docs 02

--- src/main/java/cl/lcd/controller/PricingController.java
+++ src/main/java/cl/lcd/controller/PricingController.java
@@ -2,10 +2,13 @@ package cl.lcd.controller;
 
 import cl.lcd.dto.pricing.FlightPricingConfirmRequest;
 import cl.lcd.dto.pricing.FlightPricingConfirmResponse;
+import cl.lcd.dto.search.FlightAvailabilityResponse;
 import cl.lcd.service.AmadeusPricingService;
 import com.amadeus.resources.FlightOfferSearch;
 import com.google.gson.Gson;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
@@ -31,7 +34,10 @@ public class PricingController {
 
     @PostMapping("/flights/confirm")
     @Operation(summary = "find price of flight offer search ")
-    @ApiResponse(responseCode = "200", description = " return all available flight price   [View Amadeus API Docs](https://developers.amadeus.com/self-service/category/flights/api-doc/flight-offers-price/api-reference)")
+    @ApiResponse(responseCode = "200", description = " return all available flight",
+            content = @Content(mediaType = "application/json", schema = @Schema(implementation = FlightPricingConfirmResponse.class)))
+
+    //  @ApiResponse(responseCode = "200", description = " return all available flight price   [View Amadeus API Docs](https://developers.amadeus.com/self-service/category/flights/api-doc/flight-offers-price/api-reference)")
 //    public ResponseEntity<?> searchFlightOfferPrice(@RequestBody List<Map<String, Object>> flightRequest) {
     public ResponseEntity<?> searchFlightOfferPrice(@RequestBody FlightPricingConfirmRequest flightRequest) {
         try {
